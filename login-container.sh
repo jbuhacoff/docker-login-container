60c8a218cccfb87388eaae3dced2917ddc101380
Jonathan Buhacoff <jonathan@buhacoff.net>
2018-10-13 17:30:52 -0700

    added options to select docker image and container name

--- login-container.sh
+++ login-container.sh
@@ -11,27 +11,67 @@ else
 fi
 
 label_prefix=local
+container_name=
+image=ubuntu:18.04
 
-container_id=$(docker ps --quiet --filter label=$label_prefix.user=$user)
+while [ $# -gt 0 ]
+do
+    case $1 in
+        --)
+            shift;
+            break;
+            ;;
+        --image)
+            image=$2
+            shift; shift;
+            ;;
+        --name)
+            container_name=$2
+            shift; shift;
+            ;;
+        *)
+            echo "Invalid arguments" >&2
+            exit 1
+            ;;
+    esac
+done
+
+if [ -n "$container_name" ]; then
+    # find the named container 
+    container_id=$(docker ps --quiet --filter name=$container_name)
+    label=$label_prefix.name=$container_name
+else
+    # find a default container for this user by name
+    container_id=$(docker ps --quiet --filter label=$label_prefix.user=$user)
+    label=$label_prefix.user=$user
+    container_name=${user}_container
+fi
+
 if [ -z "$container_id" ]; then
     docker run \
         --volume /home/$user/workspace:/home/$user/workspace \
         --workdir /home/$user \
-        --label $label_prefix.user=$user \
+        --label $label \
         --restart always \
-        --name ${user}_container \
+        --name $container_name \
         --user $uid:$gid \
         --interactive \
         --tty \
         --detach \
         --init \
-        $user bash
-    container_id=$(docker ps --quiet --filter label=$label_prefix.user=$user)
+        $image bash
+    container_id=$(docker ps --quiet --filter label=$label)
     container_ipaddr=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $container_id)
     
     # Custom firewall rules etc. can be setup here for the new container
     
 fi
+
+if [ -z "$container_id" ]; then
+    echo "Failed to start container" >&2
+    exit 1
+fi
+
 # login with no parameters should enter an interactive shell
 if [ $# -eq 0 ]; then
         docker exec -it $container_id /bin/bash
